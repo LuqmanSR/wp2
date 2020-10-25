39edd3e5524c502777da0bbbf40880cb1d47fb0a
LuqmanSR <Lukyrabbani@gmail.com>
2020-10-26 00:28:24 +0700

    tugas wp2

---
+++ Folder 2/Praktikum 1/kenalin.php
@@ -0,0 +1,11 @@
+<?php
+class kenalin extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> perkenalan</h1>";
+        echo "Luqman Syafi Rabbani<br>
+                saya tinggal di Desa Purwahamba<br>
+                saya suka olahraga Sepak Bola";
+    }
+}
---
+++ Folder 2/Praktikum 4/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Luqman</li>
+        <li>Nama Tengah : Syafi</li>
+        <li>Nama Belakang : Rabbani</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Ds. Purwahamba kec.Suradadi kab.Tegal</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Tegal</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Sepak Bola</li>
+    </ul>
+</section>
